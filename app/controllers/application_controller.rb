f1a30a40d54ffb034b624c42660f978b9a4e6a50
github username <github email address>
2018-11-17 18:59:48 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    erb :index
+    erb :layout
   end
 end
---
+++ db/migrate/005_artist_id_to_songs.rb
@@ -0,0 +1,5 @@
+class ArtistIdToSongs < ActiveRecord::Migration
+  def change
+    add_column :songs, :artist_id, :integer
+  end
+end
